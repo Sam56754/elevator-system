76bb5848c70e720118b9b36fda5f3c12355b6218
sam <126097333+Sam56754@users.noreply.github.com>
2024-11-18 13:55:58 +0300

    update 2

--- src/project/Elevator.java
+++ src/project/Elevator.java
@@ -16,28 +16,37 @@ public class Elevator {
 
     public Elevator(int elevatorNumber) {
         this.elevatorNumber = elevatorNumber;
-        this.currentFloor = 0;  // Assume ground floor initially
+        this.currentFloor = 0; 
         this.isMoving = false;
         this.movingDirection = "None";
     }
 
     public void stop() {
         isMoving = false;
-        System.out.println("Elevator " + elevatorNumber + " stopped.");
+        movingDirection = "None";
+        System.out.println("Elevator " + elevatorNumber + " stopped at floor " + currentFloor + ".");
     }
 
-    public void moveUp() {
-        movingDirection = "Up";
-        isMoving = true;
-        currentFloor++;
-        System.out.println("Elevator " + elevatorNumber + " moving up to floor " + currentFloor);
+    public void moveUp(int topFloor) {
+        if (currentFloor < topFloor) {
+            movingDirection = "Up";
+            isMoving = true;
+            currentFloor++;
+            System.out.println("Elevator " + elevatorNumber + " moving up to floor " + currentFloor + ".");
+        } else {
+            System.out.println("Elevator " + elevatorNumber + " is already at the top floor.");
+        }
     }
 
-    public void moveDown() {
-        movingDirection = "Down";
-        isMoving = true;
-        currentFloor--;
-        System.out.println("Elevator " + elevatorNumber + " moving down to floor " + currentFloor);
+    public void moveDown(int bottomFloor) {
+        if (currentFloor > bottomFloor) {
+            movingDirection = "Down";
+            isMoving = true;
+            currentFloor--;
+            System.out.println("Elevator " + elevatorNumber + " moving down to floor " + currentFloor + ".");
+        } else {
+            System.out.println("Elevator " + elevatorNumber + " is already at the ground floor.");
+        }
     }
 
     public void closeDoor() {
@@ -48,5 +57,39 @@ public class Elevator {
         System.out.println("Elevator " + elevatorNumber + " doors opening.");
     }
 
-    // Getters and Setters for the fields if needed
+   
+    public String getMovingDirection() {
+        return movingDirection;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public int getElevatorNumber() {
+        return elevatorNumber;
+    }
+
+    public void setElevatorNumber(int elevatorNumber) {
+        this.elevatorNumber = elevatorNumber;
+    }
+    public static void main(String[] args) {
+    // Create an elevator object
+    Elevator elevator1 = new Elevator(1);
+    
+    // Open doors, move elevator, and close doors
+    elevator1.openDoor();
+    elevator1.moveUp(10);  // Move elevator to floor 10
+    elevator1.moveDown(5);    
+    elevator1.stop();     
+    elevator1.closeDoor();  
+}
 }
